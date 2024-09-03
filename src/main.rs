829a3916b6ccd27d2977ae824158adbea0a8d8cb
Ismail Karabulut <i.karabulut93@gmail.com>
2024-09-03 12:51:37 -0500

    init w/ basic cpu emulation

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+struct CPU {
+    current_operation: u16,
+    registers: [u8; 2]
+}
+
+fn main() {
+    let mut cpu = CPU {
+        current_operation: 0,
+        registers: [0; 2]
+    };
+
+    // 8 signifies that the op involves two registers
+    // 0 maps to cpu.registers[0]
+    // 1 maps to cpu.registers[1]
+    // 4 indicates addition
+    cpu.current_operation = 0x8014;
+    
+    cpu.registers[0] = 5;
+    cpu.registers[1] = 10;
+}
